a2eecec3d0ac0a6e5885c560a0c65c0cc41fef5e
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-04-30 13:51:29 +0200

    Init commit

---
+++ lib/forthex.ex
@@ -0,0 +1,18 @@
+defmodule Forthex do
+  @moduledoc """
+  Documentation for `Forthex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Forthex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/forthex_test.exs
@@ -0,0 +1,8 @@
+defmodule ForthexTest do
+  use ExUnit.Case
+  doctest Forthex
+
+  test "greets the world" do
+    assert Forthex.hello() == :world
+  end
+end
