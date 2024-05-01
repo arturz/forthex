f13b3fccb31dc4178d735836397f0888dd17d7b9
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2024-05-01 14:41:49 +0200

    Create case with util functions for tests

---
+++ test/support/case.ex
@@ -0,0 +1,46 @@
+defmodule Forthex.Case do
+  @moduledoc false
+
+  defmacro __using__(_opts) do
+    quote do
+      use ExUnit.Case
+
+      import ExUnit.CaptureIO
+      import ExUnit.CaptureLog
+
+      alias Forthex.Interpreter.State
+
+      def create_state_with_stack(stack) do
+        State.new() |> Map.put(:stack, stack)
+      end
+
+      def create_state_with_return_stack(return_stack) do
+        State.new() |> Map.put(:return_stack, return_stack)
+      end
+
+      def assert_stack_transformation(from, to, function) when is_list(from) and is_list(to) do
+        state = create_state_with_stack(from)
+
+        new_state = function.(state)
+
+        assert new_state.stack == to
+      end
+
+      def assert_puts_true_on_stack(stack, function) do
+        state = create_state_with_stack(stack)
+        %State{stack: [value]} = function.(state)
+        assert truthy?(value)
+      end
+
+      def assert_puts_false_on_stack(stack, function) do
+        state = create_state_with_stack(stack)
+        %State{stack: [value]} = function.(state)
+        assert falsy?(value)
+      end
+
+      def falsy?(0), do: true
+      def falsy?(_value), do: false
+      def truthy?(value), do: not falsy?(value)
+    end
+  end
+end
